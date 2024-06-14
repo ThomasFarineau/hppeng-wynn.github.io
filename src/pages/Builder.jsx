6a3892c66ff469a43bc5b44061dd9160fb88d29b
Thomas <thomas.farineau@icloud.com>
2024-06-14 09:28:04 +0200

    add builder more div

--- src/pages/Builder.jsx
+++ src/pages/Builder.jsx
@@ -3,10 +3,13 @@ import Skill from "../components/builder/skill/Skill";
 
 export default function Builder() {
     return (<>
-        <h1>Builder Page</h1>
-        <p>This is the Builder page.</p>
+        <div class="level">
+            <h1>Level 1</h1>
+            <input type="number" value={1}/>
+        </div>
+        <div class="equipements">
 
-
+        </div>
         <div class="skills">
             <Skill icon={"✤"} title={"Strength"} color={"#00AA00"} input={0} assign={0} original={0} modifier={0}/>
             <Skill icon={"✦"} title={"Dexterity"} color={"#FFFF55"} input={0} assign={0} original={0} modifier={0}/>
