430db1de85d39330b9421dfdd9937877c7538eb3
Thomas Farineau <thomas.farineau@iwecloud.com>
2024-06-17 16:17:59 +0200

    try to fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,57 +1,56 @@
 import './Navbar.sass';
 
-import builderIcon from "../../assets/images/icons/default/builder.png";
-import crafterIcon from "../../assets/images/icons/default/crafter.png";
-import atlasIcon from "../../assets/images/icons/default/atlas.png";
-import devlogIcon from "../../assets/images/icons/default/devlog.png";
+import builderIcon from '../../assets/images/icons/default/builder.png';
+import crafterIcon from '../../assets/images/icons/default/crafter.png';
+import atlasIcon from '../../assets/images/icons/default/atlas.png';
+import devlogIcon from '../../assets/images/icons/default/devlog.png';
+import {A} from '@solidjs/router';
 
 function handleMouseEnter(event) {
-    const nav = event.target.closest('nav');
-    nav.classList.add('open');
+  const nav = event.target.closest('nav');
+  nav.classList.add('open');
 }
 
 function handleMouseLeave(event) {
-    const nav = event.target.closest('nav');
-    nav.classList.remove('open');
+  const nav = event.target.closest('nav');
+  nav.classList.remove('open');
 }
 
-export default function Navbar(props) {
-    const {base} = props
-    return (<nav onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        <ul>
-            <li>
-                <a href="builder">
-                    <img src={builderIcon} alt=""/>
-                    <p>Builder</p>
-                </a>
-            </li>
-            <li>
-                <a href="crafter">
-                    <img src={crafterIcon} alt=""/>
-
-                    <p>Crafter</p>
-                </a>
-            </li>
-            <li>
-                <a href="atlas">
-                    <img src={atlasIcon} alt=""/>
-
-                    <p>Atlas</p>
-                </a>
-            </li>
-            <li>
-                <a href="devlog">
-                    <img src={devlogIcon} alt=""/>
-
-                    <p>Devlog</p>
-                </a>
-            </li>
-            <li class={"bottom"}>
-                <a href="https://discord.com/invite/CGavnAnerv" target={"_blank"}>
-                    <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg" alt=""/>
-                    <p>Discord</p>
-                </a>
-            </li>
-        </ul>
-    </nav>);
+export default function Navbar() {
+  return <nav onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <ul>
+      <li>
+        <A href="builder">
+          <img src={builderIcon} alt="" />
+          <p>Builder</p>
+        </A>
+      </li>
+      <li>
+        <A href="crafter">
+          <img src={crafterIcon} alt="" />
+          <p>Crafter</p>
+        </A>
+      </li>
+      <li>
+        <A href="atlas">
+          <img src={atlasIcon} alt="" />
+          <p>Atlas</p>
+        </A>
+      </li>
+      <li>
+        <A href="devlog">
+          <img src={devlogIcon} alt="" />
+          <p>Devlog</p>
+        </A>
+      </li>
+      <li class={'bottom'}>
+        <a href="https://discord.com/invite/CGavnAnerv" target={'_blank'}>
+          <img
+            src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg"
+            alt="" />
+          <p>Discord</p>
+        </a>
+      </li>
+    </ul>
+  </nav>;
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,28 +2,29 @@ import './index.sass';
 import {render} from 'solid-js/web';
 import {Navigate, Route, Router} from '@solidjs/router';
 import Navbar from './components/navbar/Navbar';
-import Builder from "./pages/builder/Builder";
-import Crafter from "./pages/Crafter";
-import Atlas from "./pages/Atlas";
-import Devlog from "./pages/Devlog";
-import {load} from "./utils";
-const base = process.env.BASE_PATH;
+import Builder from './pages/builder/Builder';
+import Crafter from './pages/Crafter';
+import Atlas from './pages/Atlas';
+import Devlog from './pages/Devlog';
+import {load} from './utils';
 
-function App() {
-    load().then(() => console.log('loaded'));
+const base = process.env.BASE_PATH || '/';
 
-    return <>
-        <Navbar base={base}/>
-        <main>
-            <Router base={base}>
-                <Route path="" component={() => <Navigate href="builder"/>}/>
-                <Route path="builder" component={Builder}/>
-                <Route path="crafter" component={Crafter}/>
-                <Route path="atlas" component={Atlas}/>
-                <Route path="devlog" component={Devlog}/>
-            </Router>
-        </main>
-    </>;
-}
+const App = (props) => <>
+  <Navbar />
+  <main>
+    {props.children}
+  </main>
+</>;
 
-render(() => (<App/>), document.getElementById('root'));
+render(() => {
+  load().then(() => console.log(`Loaded and running on ${base}`));
+
+  return <Router root={App} base={base}>
+    <Route path="/" component={() => <Navigate href="builder" />} />
+    <Route path="/builder" component={Builder} />
+    <Route path="/crafter" component={Crafter} />
+    <Route path="/atlas" component={Atlas} />
+    <Route path="/devlog" component={Devlog} />
+  </Router>;
+}, document.getElementById('root'));
